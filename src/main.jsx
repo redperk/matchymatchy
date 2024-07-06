ee40a6cf6f633e8a326467d9fd7b20f868eac143
Jared Perkins <jperkins@nerdunited.com>
2024-07-06 13:01:09 -0600

    fix: enforce dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 
+if (typeof document !== 'undefined') {
+  document.documentElement.classList.add('dark');
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
